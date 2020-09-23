af896ea29a77232bc913d80cd021999d6b9f4b25
David Brown <dmlb2000@gmail.com>
2020-09-22 21:38:01 -0700

    Update Pipeline
    This updates the pipeline for the template repository and python
    packaging infrastructure for future packages.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@
 # -*- coding: utf-8 -*-
 """Setup and install the pacifica service."""
 from os import path
-try:  # pip version 9
-    from pip.req import parse_requirements
-except ImportError:
-    from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 setup(
     name='pacifica-service',
@@ -25,5 +19,5 @@ setup(
     author_email='dmlb2000@gmail.com',
     packages=find_packages(),
     namespace_packages=['pacifica'],
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    install_requires=[]
 )
